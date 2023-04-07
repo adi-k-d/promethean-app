ac71dd834e2a589b27910d5eac9ed5f5ae2244e6
adi-k-d <adityakdas7@gmail.com>
2023-04-07 10:38:58 +0530

    testing

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
-from db_models import db,ProductionData  
-
-engine = create_engine('http://iot-data.cn3ljizzlmy8.ap-south-1.rds.amazonaws.com/raymond_vapi_plant_performance')
-
-# create a session to interact with the database
-Session = sessionmaker(bind=engine)
-session = Session()
+from db_models import ProductionData  
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://raymond_user:uTjEw5jK2Ks2@iot-data.cn3ljizzlmy8.ap-south-1.rds.amazonaws.com:5432/raymond_plant_performance'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    try:
+        db.session.execute('SELECT 1')
+        return 'Connection successful!'
+    except:
+        return 'Connection failed.'
 
 @app.route('/upload', methods=['POST'])
 def upload():    
@@ -37,14 +41,12 @@ def upload():
           pc_dyg=row['Pc Dyg'],
           tfo=row['TFO'],
           steaming_mc=row['Steaming m/c'],
-          unmetered=row['Unmetered'],
+        #   unmetered=row['Unmetered'],
           unmetered_percentage=row['Unmetered %age'],
           dm_water=row['DM Water'],
           recovery_percentage=row['Recovery %']
       )
-      session.add(data)
-      session.commit()
-      session.close()
+      print(data)
     table = df.to_html(classes='table table-striped')    
     return render_template('table.html', table=table)
 
